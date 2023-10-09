f3b7459898f2964cd5f3dc5d0f16d12c86e2cd41
Vladislav Yurikov <yurikov.v@tutanota.com>
2023-10-09 13:05:25 +0200

    Update terraform schema & variables

--- terraform/schema.tf
+++ terraform/schema.tf
@@ -1,5 +1,6 @@
+# This block defines a variable for the BigQuery table schema.
 variable "table_schema" {
-  description = "Схема таблицы BigQuery"
+  description = "BigQuery Table Schema"
   type        = string
   default     = <<EOF
     [
@@ -7,199 +8,199 @@ variable "table_schema" {
         "name": "expand",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Строка - Expand"
+        "description": "String - Expand"
       },
       {
         "name": "id",
         "type": "INTEGER",
         "mode": "NULLABLE",
-        "description": "Целое число - ID"
+        "description": "Integer - ID"
       },
       {
         "name": "key",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Строка - Key"
+        "description": "String - Key"
       },
       {
         "name": "self",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Строка - Self"
+        "description": "String - Self"
       },
       {
         "name": "project",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Строка - Project"
+        "description": "String - Project"
       },
       {
         "name": "project_name",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Строка - Project Name"
+        "description": "String - Project Name"
       },
       {
         "name": "issue_type_name",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Строка - Issue Type Name"
+        "description": "String - Issue Type Name"
       },
       {
         "name": "resolution_name",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Строка - Resolution Name"
+        "description": "String - Resolution Name"
       },
       {
         "name": "created_date",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Дата/время - Created Date"
+        "description": "Date/Time - Created Date"
       },
       {
         "name": "resolved_date",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Дата/время - Resolved Date"
+        "description": "Date/Time - Resolved Date"
       },
       {
         "name": "severity",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Строка - Severity"
+        "description": "String - Severity"
       },
       {
         "name": "team",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Строка - Team"
+        "description": "String - Team"
       },
       {
         "name": "global_release",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Строка - Global Release"
+        "description": "String - Global Release"
       },
       {
         "name": "story_points",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Целое число - Story Points"
+        "description": "Integer - Story Points"
       },
       {
         "name": "priority",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Строка - Priority"
+        "description": "String - Priority"
       },
       {
         "name": "bug_type",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Строка - Bug Type"
+        "description": "String - Bug Type"
       },
       {
         "name": "summary",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Строка - Summary"
+        "description": "String - Summary"
       },
       {
         "name": "testing_scope",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Строка - Testing Scope"
+        "description": "String - Testing Scope"
       },
       {
         "name": "engineering_area",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Строка - Engineering Area"
+        "description": "String - Engineering Area"
       },
       {
         "name": "automation_test_name",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Строка - Automation Test Name"
+        "description": "String - Automation Test Name"
       },
       {
         "name": "status",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Строка - Status"
+        "description": "String - Status"
       },
       {
         "name": "reporter",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Строка - Reporter"
+        "description": "String - Reporter"
       },
       {
         "name": "assignee_name",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Строка - Assignee Name"
+        "description": "String - Assignee Name"
       },
       {
         "name": "total_acv",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Число с плавающей точкой - Total ACV"
+        "description": "Float - Total ACV"
       },
       {
         "name": "program",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Строка - Program"
+        "description": "String - Program"
       },
       {
         "name": "execution_comments",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Строка - Execution Comments"
+        "description": "String - Execution Comments"
       },
       {
         "name": "display_in_big_picture",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Строка - Display in Big Picture"
+        "description": "String - Display in Big Picture"
       },
       {
         "name": "updated",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Дата/время - Updated"
+        "description": "Date/Time - Updated"
       },
       {
         "name": "engineering_feedback",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Строка - Engineering Feedback"
+        "description": "String - Engineering Feedback"
       },
       {
         "name": "proposed_text_for_limitation_or_resolved_issue",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Строка - Proposed Text for Limitation or Resolved Issue"
+        "description": "String - Proposed Text for Limitation or Resolved Issue"
       },
       {
         "name": "customer_request_type",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Строка - Customer Request Type"
+        "description": "String - Customer Request Type"
       },
       {
         "name": "project_type",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Строка - Project Type"
+        "description": "String - Project Type"
       },
       {
         "name": "channel",
         "type": "STRING",
         "mode": "NULLABLE",
-        "description": "Строка - Channel"
+        "description": "String - Channel"
       }
     ]
   EOF
